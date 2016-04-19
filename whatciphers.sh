1f0a1d14dd8c66ad0b227df24821585d05ec553b
Sean Davis <dive@hq.endersgame.net>
2016-04-19 11:54:32 -0500

    Update license blurbs

--- whatciphers.sh
+++ whatciphers.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-# Author:   Sean Davis <sdigit@github.com>
-# License:  Apache 2.0
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0, and the BSD License. See the LICENSE file in the root of this repository
+# for complete details.
+
 usage()
 {
     echo "Usage: $0 <webserver:port>"
